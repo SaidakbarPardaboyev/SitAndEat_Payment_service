7416790b9202144093daec05b68472da70d60bd6
pronium <azizbekqobulov05@gmail.com>
2024-07-11 11:01:59 +0500

    logger done

--- service/payment.go
+++ service/payment.go
@@ -3,24 +3,26 @@ package service
 import (
 	"context"
 	"database/sql"
-	"log"
+	"log/slog"
 	pb "payments/genproto/payment"
+	"payments/pkg/logger"
 	"payments/storage/postgres"
 )
 
 type NewPaymentService struct {
 	pb.UnimplementedPaymentServer
 	Payment *postgres.PaymentRepo
+	Logger  *slog.Logger
 }
 
 func NewPaymentServiceRepo(db *sql.DB) *NewPaymentService {
-	return &NewPaymentService{Payment: postgres.NewPaymentRepo(db)}
+	return &NewPaymentService{Payment: postgres.NewPaymentRepo(db), Logger: logger.NewLogger()}
 }
 
 func (P *NewPaymentService) CreatePayments(ctx context.Context, req *pb.CreatePayment) (*pb.Status, error) {
 	resp, err := P.Payment.CreatePayments(req)
 	if err != nil {
-		log.Fatalf("Payment create error: %v", err)
+		P.Logger.Error("Payment create error: %v", err)
 		return nil, err
 	}
 	return resp, nil
@@ -29,7 +31,7 @@ func (P *NewPaymentService) CreatePayments(ctx context.Context, req *pb.CreatePa
 func (P *NewPaymentService) GetByIdPayments(ctx context.Context, req *pb.GetById) (*pb.GetByIdResponse, error) {
 	resp, err := P.Payment.GetPaymentStatusById(req)
 	if err != nil {
-		log.Fatalf("Read payment error: %v", err)
+		P.Logger.Error("Read payment error: %v", err)
 		return nil, err
 	}
 	return resp, nil
@@ -38,7 +40,7 @@ func (P *NewPaymentService) GetByIdPayments(ctx context.Context, req *pb.GetById
 func (P *NewPaymentService) UpdatePayments(ctx context.Context, req *pb.UpdatePayment) (*pb.Status, error) {
 	resp, err := P.Payment.UpdatePayments(req)
 	if err != nil {
-		log.Fatalf("Update payment error: %v", err)
+		P.Logger.Error("Update payment error: %v", err)
 		return nil, err
 	}
 	return resp, nil
